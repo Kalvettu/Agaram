5d90bf454dc759dd5df028b9e9bcfb07001512f1
Sivasubramanyam A <sivasubramanyama@gmail.com>
2016-02-07 17:43:32 +0530

    Add static content

---
+++ static/js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+    var temp_image, first_save;
+
+    $('#image-preview').click(function (argument) {
+
+        var formData = new FormData($('#image-upload')[0]);
+        $.ajax({
+
+            url : '/upload',
+            dataType: 'json',
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function(response) {
+                console.log(response);
+                if (response['error'] == 0) {
+                    temp_image = response['url'];
+                    $('#temporary').attr('src', temp_image);
+                };
+            }
+
+        });
+
+    });
+
+
+    $('#image-submit').click(function (argument) {
+
+        var formData = new FormData($('#image-upload')[0]);
+        formData.append('generate', 'yes');
+
+        $.ajax({
+
+            url : '/upload',
+            dataType: 'json',
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function(response) {
+                console.log(response);
+            }
+
+        });
+
+    });
+
+});
--- views.py
+++ views.py
@@ -3,9 +3,12 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from time import time
-from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+from datetime import datetime
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template, jsonify
 from werkzeug import secure_filename
 
+
+TEMP_FOLDER = 'static/temporary/'
 UPLOAD_FOLDER = 'static/uploads/'
 SEGMENTED_INSCRIPTIONS = 'static/segmented/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
@@ -23,82 +26,45 @@ def uploaded_file(filename):
                                filename)
 
 @app.route('/', methods = ['GET', 'POST'])
+def index():
+	return render_template('index.html')
+
+@app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
 
 	if request.method == 'POST':
+
 		file = request.files['file']
+		response = {}
 
 		if file and allowed_file(file.filename):
-			
-			filename = secure_filename(file.filename)
+			print 'im of'
+			now = datetime.now()
+			filename = now.strftime('%Y-%m-%d-%H-%M-%S') + '_' + secure_filename(file.filename) 
 			file.save(os.path.join(UPLOAD_FOLDER, filename))
-
-			#convert here into segmented letters
-
+			response['first_save'] = url_for('uploaded_file', filename=filename)
 			#load image
 			dir = os.curdir
 			img = filename
+			print img
 			path = os.path.join(UPLOAD_FOLDER,img)
 			raw_image = cv2.imread(path,0)
 			
 			#blur image to remove noise
 			#sm_image = cv2.medianBlur(raw_image, 3)
+			threshold = int(request.form['threshold'])
 			sm_image = cv2.bilateralFilter(raw_image, 25, 50, 50)
-			ret,bw_image = cv2.threshold(sm_image,160,255,cv2.THRESH_BINARY_INV)
-			
-			#perform erosion and dilation to enhance noise removal
-			kernel = np.ones((1.5,1.5),np.uint8)
-			er_image = cv2.erode(bw_image,kernel)
-			kernel = np.ones((2,2),np.uint8)
-			di_image = cv2.dilate(er_image,kernel, iterations=1)
-			
-			#find contours
-			mo_image = di_image.copy()
-			contour0 = cv2.findContours(mo_image.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-			contours = [cv2.approxPolyDP(cnt,3,True) for cnt in contour0[0]]
-			
-			maxArea = 0
-			rect=[]
-			for ctr in contours:
-			    maxArea = max(maxArea,cv2.contourArea(ctr))
+			ret,bw_image = cv2.threshold(sm_image,threshold,255,cv2.THRESH_BINARY_INV)
+			print 'check'
+			cv2.imwrite(os.path.join(TEMP_FOLDER, filename), bw_image)
+			return jsonify({
+				'url': 'static/temporary/'+filename,
+				'error': 0,
+				'threshold': threshold
+			})
 
-			areaRatio = 0.05
-
-			for ctr in contours:    
-			    if cv2.contourArea(ctr) > maxArea * areaRatio: 
-			        rect.append(cv2.boundingRect(cv2.approxPolyDP(ctr,1,True)))
-			
-			symbols=[]
-			for i in rect:
-			    x = i[0]
-			    y = i[1]
-			    w = i[2]
-			    h = i[3]
-			    p1 = (x,y)
-			    p2 = (x+w,y+h)
-			    cv2.rectangle(mo_image,p1,p2,255,2)
-			    image = cv2.resize(mo_image[y:y+h,x:x+w],(32,32))
-			    symbols.append(image.reshape(1024,).astype("uint8"))
-			
-			#segment images and export them
-			testset_data = np.array(symbols)
-			#plt.show()
-			# show glyphs
-			img_name_base = str(time()) + '_'
-			for i in range(len(symbols)):
-				image = np.zeros(shape=(64,64))
-				image[15:47,15:47] = symbols[i].reshape((32,32))
-				segment_name = img_name_base + str(i) + '.jpg'
-				cv2.imwrite(os.path.join(SEGMENTED_INSCRIPTIONS, segment_name), image)
-			return redirect(url_for('uploaded_file', filename=filename))
-
-	return '''
-	<!DOCTYPE html>
-	<form action="" method="post" enctype="multipart/form-data">
-		<input type="file" name="file">
-		<input type="submit" value="Upload">
-	</form>
-	'''
+	else:
+		return jsonify({'error': 1})
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0', port=5000)
